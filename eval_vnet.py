471e88663247fe263de1c8da24f07f396c29304d
Hua Yu <hyu2000@gmail.com>
2021-05-25 13:51:42 -0400

    roughly works

--- eval_vnet.py
+++ eval_vnet.py
@@ -29,29 +29,38 @@ class ScoreStats(object):
 
     def __init__(self):
         self.num_games = 0
-        self.max_steps = 0
-        self.tromp_counters = np.zeros(NUM_TURNS_COUNTED)
-        self.vnet_counters  = np.zeros(NUM_TURNS_COUNTED)
         self.tromp_final = 0
         self.vnet_final  = 0
+        self.tromp_counters = np.zeros(NUM_TURNS_COUNTED)
+        self.vnet_counters  = np.zeros(NUM_TURNS_COUNTED)
+        self.total_counters = np.zeros(NUM_TURNS_COUNTED)
+
+    @staticmethod
+    def _pad_np_1darray(arr: np.array, n: int, const_val=0):
+        pad_right = n - len(arr)
+        assert len(arr.shape) == 1 and pad_right >= 0
+        return np.pad(arr, [(0, pad_right)], constant_values=const_val)
 
     def add_game(self, win_loss: int, tromp_final: int, vnet_final: int,
                  tromp_binary_by_turns: List[int], vnet_binary_by_turns: List[int]):
         assert len(tromp_binary_by_turns) <= NUM_TURNS_COUNTED
         self.num_games += 1
-        self.max_steps = max(len(tromp_binary_by_turns), self.max_steps)
+        num_steps = len(tromp_binary_by_turns)
 
-        self.tromp_counters += np.array(tromp_binary_by_turns == win_loss)
-        self.vnet_counters  += np.array( vnet_binary_by_turns == win_loss)
+        self.tromp_counters += self._pad_np_1darray(np.array(tromp_binary_by_turns) == win_loss, NUM_TURNS_COUNTED)
+        self.vnet_counters  += self._pad_np_1darray(np.array( vnet_binary_by_turns) == win_loss, NUM_TURNS_COUNTED)
+        self.total_counters += self._pad_np_1darray(np.ones(num_steps), NUM_TURNS_COUNTED)
         self.tromp_final += tromp_final == win_loss
         self.vnet_final  +=  vnet_final == win_loss
 
     def report(self):
         logging.info('Total %d games, final acc: Tromp %.2f, vnet %.2f',
                      self.num_games, self.tromp_final / self.num_games, self.vnet_final / self.num_games)
+        max_steps = (self.total_counters > 0).sum()
+        total_counts = self.total_counters[:max_steps]
         logging.info('By turns:\n\tTromp %s\n\tvnet  %s',
-                     ['%.2f' % x for x in self.tromp_counters / self.num_games],
-                     ['%.2f' % x for x in self.vnet_counters  / self.num_games])
+                     ['%.2f' % x for x in self.tromp_counters[:max_steps] / total_counts],
+                     ['%.2f' % x for x in self.vnet_counters[ :max_steps] / total_counts])
 
 
 def run_game(network, game_id, reader: SGFReader, stats: ScoreStats):
@@ -65,7 +74,7 @@ def run_game(network, game_id, reader: SGFReader, stats: ScoreStats):
     pos_to_eval = []  # type: List[go.Position]
     for pwc in reader.iter_pwcs():
         position = pwc.position
-        if position % 10 == 0:
+        if position.n % 10 == 0:
             pos_to_eval.append(position)
     # last position
     pos_to_eval.append(pwc.position)
@@ -77,18 +86,19 @@ def run_game(network, game_id, reader: SGFReader, stats: ScoreStats):
     assert len(tromp_scores) == len(vnet_scores)
 
     tromp_final = tromp_binary.pop()
-    vnet_final = vnet_binary.pop()
+    vnet_final  = vnet_binary.pop()
     stats.add_game(win_loss, tromp_final, vnet_final, tromp_binary, vnet_binary)
 
 
 def run_games(start_idx=0):
     """
     """
-    store = GameStore(data_dir=FLAGS.tar_dir)
+    # store = GameStore(data_dir=FLAGS.tar_dir)
+    store = GameStore(data_dir=f'{myconf.DATA_DIR}')
     game_iter = store.game_iter([store.ds_pro, store.ds_top], filter_game=True)
 
-    # model_file = f'{myconf.MODELS_DIR}/model3_epoch_5.h5'
-    model_file = FLAGS.load_file
+    # model_file = FLAGS.load_file
+    model_file = f'{myconf.MODELS_DIR}/model3_epoch_5.h5'
     network = dual_net.DualNetwork(model_file)
     stats = ScoreStats()
     for game_idx, (game_id, reader) in enumerate(game_iter):
@@ -96,6 +106,8 @@ def run_games(start_idx=0):
             continue
 
         run_game(network, game_id, reader, stats)
+        # if game_idx > 3:
+        #     break
     stats.report()
 
 
