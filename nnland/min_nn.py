a2b19c2a0f3fef0a0f5cdb70ba537a4333dc4fdd
Hua Yu <hyu2000@gmail.com>
2021-05-31 11:24:26 -0400

    when limited to [0, 1000]: rmse drops to 0.3x after 30 epochs

--- nnland/min_nn.py
+++ nnland/min_nn.py
@@ -17,15 +17,15 @@ def nn2(num_hidden_nodes):
 
 
 def train():
-    LIMIT1, LIMIT2 = -1000, 1000
-    N, N_train = 10000, 9000
+    LIMIT1, LIMIT2 = 0, 1000
+    N = 10000
     x = np.random.randint(LIMIT1, LIMIT2, (N, 2))
     y = np.min(x, axis=1)
-    x_train, x_val = x[:N_train, :], x[N_train:, :]
-    y_train, y_val = y[:N_train], y[N_train:]
+    IDX_VAL0 = -1000
+    x_train, x_val = x[:IDX_VAL0, :], x[IDX_VAL0:, :]
+    y_train, y_val = y[:IDX_VAL0], y[IDX_VAL0:]
     model = nn2(4)
-    history = model.fit(x_train, y_train, validation_data=(x_val, y_val), batch_size=32,
-                        epochs=5)
+    history = model.fit(x_train, y_train, validation_data=(x_val, y_val), batch_size=32, epochs=100)
     print(history.history)
 
 
