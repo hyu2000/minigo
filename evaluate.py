04b7cb7dbec8371ff453f01dc85bad8f1dbb547d
Hua Yu <hyu2000@gmail.com>
2021-07-20 16:04:37 -0400

    refactor evaluate. no logic changes

--- evaluate.py
+++ evaluate.py
@@ -37,7 +37,7 @@ flags.declare_key_flag('verbose')
 FLAGS = flags.FLAGS
 
 
-def play_match(black_model, white_model, games, sgf_dir):
+def play_tournament(black_model, white_model, num_games, sgf_dir):
     """Plays matches between two neural nets.
 
     Args:
@@ -56,72 +56,75 @@ def play_match(black_model, white_model, games, sgf_dir):
     black_name = os.path.basename(black_net.save_file)
     white_name = os.path.basename(white_net.save_file)
 
-    for i in range(games):
-        num_move = 0  # The move number of the current game
+    for i in range(num_games):
+        active = play_game(black, white)
 
-        for player in [black, white]:
-            player.initialize_game()
-            first_node = player.root.select_leaf()
-            prob, val = player.network.run(first_node.position)
-            first_node.incorporate_results(prob, val, first_node)
+        fname = "{:d}-{:s}-vs-{:s}-{:d}.sgf".format(int(time.time()),
+                                                    white_name, black_name, i)
+        if active.result == 0:
+            active.set_result(active.root.position.result(), was_resign=False)
+        game_history = active.position.recent
+        with gfile.GFile(os.path.join(sgf_dir, fname), 'w') as _file:
+            sgfstr = sgf_wrapper.make_sgf(game_history,
+                                          active.result_string, komi=active.position.komi,
+                                          black_name=black_name, white_name=white_name)
+            _file.write(sgfstr)
+        move_history_head = ' '.join([coords.to_gtp(game_history[i].move) for i in range(5)])
+        print(f'Finished game {i}: #moves=%d %d %d {active.result_string} %s' % (
+            len(game_history), black.num_readouts, white.num_readouts, move_history_head))
 
-        while True:
-            start = time.time()
-            active   = white if num_move % 2 else black
-            inactive = black if num_move % 2 else white
 
-            current_readouts = active.root.N
-            while active.root.N < current_readouts + active.num_readouts:
-                active.tree_search()
+def play_game(black, white) -> MCTSPlayer:
+    num_move = 0  # The move number of the current game
+    for player in [black, white]:
+        player.initialize_game()
+        first_node = player.root.select_leaf()
+        prob, val = player.network.run(first_node.position)
+        first_node.incorporate_results(prob, val, first_node)
+    while True:
+        start = time.time()
+        active = white if num_move % 2 else black
+        inactive = black if num_move % 2 else white
 
-            # print some stats on the search
-            if FLAGS.verbose >= 3:
-                print(active.root.position)
+        current_readouts = active.root.N
+        while active.root.N < current_readouts + active.num_readouts:
+            active.tree_search()
 
-            # First, check the roots for hopeless games.
-            if active.should_resign():  # Force resign
-                active.set_result(-1 *
-                                  active.root.position.to_play, was_resign=True)
-                inactive.set_result(
-                    active.root.position.to_play, was_resign=True)
+        # print some stats on the search
+        if FLAGS.verbose >= 3:
+            print(active.root.position)
 
-            if active.is_done():
-                fname = "{:d}-{:s}-vs-{:s}-{:d}.sgf".format(int(time.time()),
-                                                            white_name, black_name, i)
-                if active.result == 0:
-                    active.set_result(active.root.position.result(), was_resign=False)
-                game_history = active.position.recent
-                with gfile.GFile(os.path.join(sgf_dir, fname), 'w') as _file:
-                    sgfstr = sgf_wrapper.make_sgf(game_history,
-                                                  active.result_string, komi=active.position.komi,
-                                                  black_name=black_name, white_name=white_name)
-                    _file.write(sgfstr)
-                move_history_head = ' '.join([coords.to_gtp(game_history[i].move) for i in range(5)])
-                print(f'Finished game {i}: #moves={num_move} %d %d {active.result_string} %s' % (
-                    black.num_readouts, white.num_readouts, move_history_head))
-                break
+        # First, check the roots for hopeless games.
+        if active.should_resign():  # Force resign
+            active.set_result(-1 *
+                              active.root.position.to_play, was_resign=True)
+            inactive.set_result(
+                active.root.position.to_play, was_resign=True)
 
-            move = active.pick_move()
-            active.play_move(move)
-            inactive.play_move(move)
+        if active.is_done():
+            return active
 
-            dur = time.time() - start
-            num_move += 1
+        move = active.pick_move()
+        active.play_move(move)
+        inactive.play_move(move)
 
-            if (FLAGS.verbose > 1):  # or (FLAGS.verbose == 1 and num_move % 10 == 9):
-                timeper = (dur / readouts) * 100.0
-                print(active.root.position)
-                print("%d: %d readouts, %.3f s/100. (%.2f sec)" % (num_move,
-                                                                   active.num_readouts,
-                                                                   timeper,
-                                                                   dur))
+        dur = time.time() - start
+        num_move += 1
+
+        if (FLAGS.verbose > 1):  # or (FLAGS.verbose == 1 and num_move % 10 == 9):
+            timeper = (dur / active.num_readouts) * 100.0
+            print(active.root.position)
+            print("%d: %d readouts, %.3f s/100. (%.2f sec)" % (num_move,
+                                                               active.num_readouts,
+                                                               timeper,
+                                                               dur))
 
 
 def main(argv):
     """Play matches between two neural nets."""
     _, black_model, white_model = argv
     utils.ensure_dir_exists(FLAGS.eval_sgf_dir)
-    play_match(black_model, white_model, FLAGS.num_evaluation_games, FLAGS.eval_sgf_dir)
+    play_tournament(black_model, white_model, FLAGS.num_evaluation_games, FLAGS.eval_sgf_dir)
     # play_match(f'{myconf.MODELS_DIR}/endgame1_epoch_2.h5', f'{myconf.MODELS_DIR}/endgame1_epoch_2.h5',
     #            FLAGS.num_evaluation_games, FLAGS.eval_sgf_dir)
 
