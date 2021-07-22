d1739123bdc68d230973259be80016cff61003a5
Hua Yu <hyu2000@gmail.com>
2021-07-22 14:43:05 -0400

    selfplay log mystery

--- run_selfplay.sh
+++ run_selfplay.sh
@@ -3,6 +3,8 @@ DRIVE_HOME=/content/drive/MyDrive/dlgo
 
 export BOARD_SIZE=5
 
+#  --dirichlet_noise_weight=0.025 \
+
 python3 run_selfplay.py \
   --verbose=0 \
   --selfplay_dir="${DRIVE_HOME}/selfplay/train" \
@@ -13,5 +15,3 @@ python3 run_selfplay.py \
   --load_file="${DRIVE_HOME}/checkpoints/model5s_epoch_2.h5" \
   --num_games=2000 \
   2>&1 | tee "${DRIVE_HOME}/selfplay/run_selfplay.log"
-
-#  --dirichlet_noise_weight=0.025 \
