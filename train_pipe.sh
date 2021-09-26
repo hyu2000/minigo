f7d727e68648215ce836dafe3437a57f657f353d
Hua Yu <hyu2000@gmail.com>
2021-09-26 14:36:41 -0400

    selfplay9 to eval model9_1

--- train_pipe.sh
+++ train_pipe.sh
@@ -8,7 +8,7 @@ MODEL_DIR="${DRIVE_HOME}/checkpoints"
 LOG_DIR="${DRIVE_HOME}/logs"
 
 # bash 3 supports range
-for i in {8..9}
+for i in {9..9}
 do
   SELFPLAY_DIR="${LOCAL_HOME}/selfplay${i}"
   echo "selfplay: ${SELFPLAY_DIR}"
@@ -22,27 +22,27 @@ do
   --holdout_dir="${SELFPLAY_DIR}/val" \
   --sgf_dir="${SELFPLAY_DIR}/sgf" \
   --holdout_pct=0 \
-  --load_file="${MODEL_DIR}/model${i}_epoch2.h5" \
+  --load_file="${MODEL_DIR}/model${i}_epoch1.h5" \
   --softpick_move_cutoff=20 \
   --dirichlet_noise_weight=0.125 \
-  --num_games_share_tree=50 \
+  --num_games_share_tree=100 \
   --num_readouts=200 \
   --parallel_readouts=16 \
-  --num_games=2000 \
+  --num_games=1000 \
   2>&1 | tee "${LOG_DIR}/selfplay${i}.log"
 
   if [ $? -ne 0 ]; then
       break
   fi
 
-  python3 enhance_ds.py ${SELFPLAY_DIR} | tee "${LOG_DIR}/enhance${i}.log" 2>&1
+  python3 enhance_ds.py ${SELFPLAY_DIR} 2>&1 | tee "${LOG_DIR}/enhance${i}.log"
 
   # save data to drive?
   if [ $? -ne 0 ]; then
       break
   fi
 
-  python3 run_train.py "${SELFPLAY_DIR}/train" ${MODEL_DIR} $i | tee "${LOG_DIR}/train${i}.log" 2>&1
+  python3 run_train.py "${SELFPLAY_DIR}/train" ${MODEL_DIR} $i 2>&1 | tee "${LOG_DIR}/train${i}.log"
 
   if [ $? -ne 0 ]; then
       break
