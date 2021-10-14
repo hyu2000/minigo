ee1aed7a9634cb7e577453f34a14bdac5b6189c7
Hua Yu <hyu2000@gmail.com>
2021-10-14 10:24:45 -0400

    to see how long a selfplay game takes in 9x9

--- run_selfplay.sh
+++ run_selfplay.sh
@@ -1,21 +1,22 @@
 DRIVE_HOME=/content/drive/MyDrive/dlgo
-#DRIVE_HOME=/Users/hyu/PycharmProjects/dlgo/5x5
+DRIVE_HOME=/Users/hyu/PycharmProjects/dlgo/9x9
 
 export BOARD_SIZE=9
 
-mkdir ${DRIVE_HOME}/selfplay
+#mkdir ${DRIVE_HOME}/selfplay
 
 python3 run_selfplay.py \
   --verbose=0 \
   --selfplay_dir="${DRIVE_HOME}/selfplay/train" \
   --holdout_dir="${DRIVE_HOME}/selfplay/val" \
   --sgf_dir="${DRIVE_HOME}/selfplay/sgf" \
-  --load_file="${DRIVE_HOME}/checkpoints/model9_epoch_3.h5" \
-  --softpick_move_cutoff=6 \
+  --softpick_move_cutoff=20 \
   --num_readouts=200 \
   --parallel_readouts=16 \
-  --num_games=2000 \
+  --num_games_share_tree=1 \
+  --num_games=5 \
   2>&1 | tee "${DRIVE_HOME}/selfplay/run_selfplay.log"
 
+#  --load_file="${DRIVE_HOME}/checkpoints/model1_epoch2.h5" \
 #  --dirichlet_noise_weight=0.025 \
 
