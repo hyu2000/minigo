7eacaa6d1846e05c1702a7451e5b103751bd443a
Hua Yu <hyu2000@gmail.com>
2021-11-06 21:35:53 -0400

    parallelize run_selfplay; increase dir-noise

--- run_selfplay.sh
+++ run_selfplay.sh
@@ -5,19 +5,21 @@ export BOARD_SIZE=9
 
 #mkdir ${DRIVE_HOME}/selfplay
 
-python3 run_selfplay.py \
-  --verbose=0 \
-  --selfplay_dir="${DRIVE_HOME}/selfplay/train" \
-  --holdout_dir="${DRIVE_HOME}/selfplay/val" \
-  --sgf_dir="${DRIVE_HOME}/selfplay/sgf" \
-  --softpick_move_cutoff=8 \
-  --num_readouts=400 \
-  --parallel_readouts=16 \
-  --holdout_pct=0 \
-  --num_games_share_tree=1 \
-  --dirichlet_noise_weight=0.025 \
-  --load_file="${DRIVE_HOME}/checkpoints/model9_epoch2.h5" \
-  --num_games=100 \
-  2>&1 | tee "${DRIVE_HOME}/selfplay/run_selfplay.log"
+for i in {1..2}
+do
+  python3 run_selfplay.py \
+    --verbose=0 \
+    --selfplay_dir="${DRIVE_HOME}/selfplay/train" \
+    --holdout_dir="${DRIVE_HOME}/selfplay/val" \
+    --sgf_dir="${DRIVE_HOME}/selfplay/sgf" \
+    --softpick_move_cutoff=8 \
+    --num_readouts=400 \
+    --parallel_readouts=16 \
+    --holdout_pct=0 \
+    --num_games_share_tree=1 \
+    --dirichlet_noise_weight=0.05 \
+    --load_file="${DRIVE_HOME}/checkpoints/model10_epoch4.h5" \
+    --num_games=500 \
+    2>&1 | tee "${DRIVE_HOME}/selfplay/run_selfplay${i}.log" &
 
-
+done
