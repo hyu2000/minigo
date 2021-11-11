784160593f792b92b25279054989487eae2eca2e
Hua Yu <hyu2000@gmail.com>
2021-11-11 07:40:51 -0500

    selfplay on my triple-inited model

--- run_selfplay.sh
+++ run_selfplay.sh
@@ -3,24 +3,25 @@ DRIVE_HOME=/Users/hyu/PycharmProjects/dlgo/9x9
 
 export BOARD_SIZE=9
 
-#mkdir ${DRIVE_HOME}/selfplay
+SELFPLAY_DIR="${DRIVE_HOME}/selfplay"
+#mkdir ${SELFPLAY_DIR}
 
-for i in {0..0}
+for i in {1..4}
 do
   python3 run_selfplay.py \
     --verbose=0 \
-    --selfplay_dir="${DRIVE_HOME}/selfplay/train" \
-    --holdout_dir="${DRIVE_HOME}/selfplay/val" \
-    --sgf_dir="${DRIVE_HOME}/selfplay/sgf" \
+    --selfplay_dir="${SELFPLAY_DIR}/train" \
+    --holdout_dir="${SELFPLAY_DIR}/val" \
+    --sgf_dir="${SELFPLAY_DIR}/sgf" \
     --softpick_move_cutoff=8 \
-    --num_readouts=100 \
+    --num_readouts=400 \
     --parallel_readouts=16 \
     --holdout_pct=0 \
     --num_games_share_tree=1 \
     --dirichlet_noise_weight=0 \
-    --num_games=1000 \
-    --resign_threshold=-1 \
-    2>&1 | tee "${DRIVE_HOME}/selfplay/run_selfplay${i}.log" &
-#    --load_file="${DRIVE_HOME}/checkpoints/model10_epoch4.h5" \
+    --num_games=300 \
+    --load_file="${DRIVE_HOME}/pbt/model4_epoch3.h5" \
+    --resign_threshold=-0.99 \
+    2>&1 | tee "${SELFPLAY_DIR}/run_selfplay${i}.log" &
 
 done
