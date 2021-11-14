261eaba02cd3dcb088698fa3ddd8ff45c77c07a8
Hua Yu <hyu2000@gmail.com>
2021-11-14 11:10:49 -0500

    small enhancement

--- evaluate_pbt.py
+++ evaluate_pbt.py
@@ -21,6 +21,9 @@ from sgf_wrapper import SGFReader
 import myconf
 
 
+MIN_NUM_GAMES_SIDED = 3
+
+
 def scan_results(sgfs_dir: str) -> pd.DataFrame:
     """ scan sgfs to build the model comparison matrix """
     game_counts = defaultdict(lambda: defaultdict(int))
@@ -39,12 +42,16 @@ def scan_results(sgfs_dir: str) -> pd.DataFrame:
         game_counts[black_id][white_id] += 1
         black_wins[black_id][white_id] += result_sign == 1
 
-    # transform into our target df
+    # construct df: sided first
     models = sorted(models)
-    df_counts = pd.DataFrame(game_counts, index=models, columns=models).T.fillna(0).astype(int)
+    df_counts_raw = pd.DataFrame(game_counts, index=models, columns=models)
+    assert (df_counts_raw.fillna(MIN_NUM_GAMES_SIDED) >= MIN_NUM_GAMES_SIDED).all().all()
+    df_counts = df_counts_raw.T.fillna(0).astype(int)
     df_blackwins = pd.DataFrame(black_wins, index=models, columns=models).T.fillna(0).astype(int)
     # make sure df_counts == df_counts.T for black/white parity, as well as min #games is played
     pd.testing.assert_frame_equal(df_counts, df_counts.T)
+    total_num_games = df_counts.sum().sum()
+    print(f'Found {total_num_games} games')
 
     # now merge sided stats into a single stat: upper-triangle only, ignore the lower half (although it has a natural meaning)
     df_counts2 = df_counts + df_counts.T
@@ -58,7 +65,7 @@ def scan_results(sgfs_dir: str) -> pd.DataFrame:
 
 def main():
     df = scan_results(f'{myconf.EXP_HOME}/eval_bots/sgfs')
-    df.to_pickle('/tmp/df.pickle')
+    df.to_pickle('/tmp/df.pkl')
 
 
 if __name__ == '__main__':
