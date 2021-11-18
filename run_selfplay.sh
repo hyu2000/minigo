ed4643c956f56d70b30081212190bc0877923ffd
Hua Yu <hyu2000@gmail.com>
2021-11-17 21:29:38 -0500

    first run w/ playcap randomization

--- run_selfplay.sh
+++ run_selfplay.sh
@@ -6,7 +6,7 @@ export BOARD_SIZE=9
 SELFPLAY_DIR="${DRIVE_HOME}/selfplay"
 #mkdir ${SELFPLAY_DIR}
 
-for i in {1..4}
+for i in {6..7}
 do
   python3 run_selfplay.py \
     --verbose=0 \
@@ -15,13 +15,15 @@ do
     --sgf_dir="${SELFPLAY_DIR}/sgf" \
     --softpick_move_cutoff=6 \
     --num_readouts=600 \
+    --num_fast_readouts=100 \
+    --full_readout_prob=0.25 \
     --parallel_readouts=16 \
     --holdout_pct=0 \
     --num_games_share_tree=1 \
-    --dirichlet_noise_weight=0.10 \
+    --dirichlet_noise_weight=0.125 \
     --num_games=500 \
-    --load_file="${DRIVE_HOME}/pbt/model10_epoch1.h5" \
-    --resign_threshold=-0.999 \
+    --load_file="${DRIVE_HOME}/pbt/model13_epoch1.h5" \
+    --resign_threshold=-1 \
     2>&1 | tee "${SELFPLAY_DIR}/run_selfplay${i}.log" &
 
 done
