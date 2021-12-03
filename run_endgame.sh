7f44a59d10745a65b8bfaa82203a4cea9a99c4cb
Hua Yu <hyu2000@gmail.com>
2021-12-02 21:01:04 -0500

    playcap randomization

--- run_endgame.sh
+++ run_endgame.sh
@@ -6,7 +6,7 @@ export BOARD_SIZE=9
 SELFPLAY_DIR="${DRIVE_HOME}/selfplay"
 mkdir ${SELFPLAY_DIR}
 
-for i in {1..3}
+for i in {1..4}
 do
   python3 run_endgame.py \
     --verbose=0 \
@@ -17,12 +17,12 @@ do
     --softpick_move_cutoff=6 \
     --num_readouts=600 \
     --num_fast_readouts=100 \
-    --full_readout_prob=1. \
+    --full_readout_prob=0.25 \
     --parallel_readouts=16 \
     --holdout_pct=0 \
     --dirichlet_noise_weight=0.25 \
-    --num_games=2 \
-    --load_file="${DRIVE_HOME}/pbt/model16_epoch1.h5" \
+    --num_games=2000 \
+    --load_file="${DRIVE_HOME}/pbt/endgame26_epoch1.h5" \
     --resign_threshold=-1 \
     2>&1 | tee "${SELFPLAY_DIR}/run_selfplay${i}.log" &
 
