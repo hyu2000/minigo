552747d57b3b0640af9293f925751ded89395a44
Hua Yu <hyu2000@gmail.com>
2021-12-04 16:06:02 -0500

    Benson's algo: renaming, Region

--- benson.py
+++ benson.py
@@ -15,6 +15,7 @@ Implementation:
   or wall.  This is similar to a chain, just that it's the maximal region of empty+white
 
 """
+from collections import namedtuple
 from typing import Tuple, Dict
 import numpy as np
 
@@ -25,44 +26,59 @@ from tests import test_utils
 from tests.test_go import coords_from_gtp_set
 
 
+class Region(namedtuple('Group', ['id', 'stones', 'liberties', 'chains', 'color'])):
+    """
+    stones: a frozenset of Coordinates belonging to this group
+    liberties: a frozenset of Coordinates that are empty and adjacent to this group.
+    color: color of this group
+    """
+
+
 class PassAliveTracker:
     """
     - how we will use it:
     - is it easy to incrementally update its status?
     """
     def __init__(self):
-        self.group_index = -np.ones([go.N, go.N], dtype=np.int32)  # type: np.ndarray
-        self.groups = dict()  # type: Dict[int, Group]
-        self.max_group_id = 0
+        self.region_index = -np.ones([go.N, go.N], dtype=np.int32)  # type: np.ndarray
+        self.regions = dict()  # type: Dict[int, Region]
+        self.max_region_id = 0
+        self.lib_tracker = None
 
     @staticmethod
     def from_board(board: np.ndarray, color_bound) -> 'PassAliveTracker':
-        board = np.copy(board)
-        curr_group_id = 0
         tracker = PassAliveTracker()
+        lib_tracker = LibertyTracker.from_board(board)
+
+        board = np.copy(board)
+        curr_region_id = 0
         for color in (go.EMPTY,):
             while color in board:
-                curr_group_id += 1
+                curr_region_id += 1
                 found_color = np.where(board == color)
                 coord = found_color[0][0], found_color[1][0]
-                chain, reached = go.find_maximal_region_with_no(board, coord, color_bound)
-                liberties = frozenset(r for r in reached if board[r] == go.EMPTY)
-                new_group = go.Group(curr_group_id, frozenset(chain), liberties, color)
-                tracker.groups[curr_group_id] = new_group
-                for s in chain:
-                    tracker.group_index[s] = curr_group_id
-                go.place_stones(board, go.FILL, chain)
+                region, reached = go.find_maximal_region_with_no(board, coord, color_bound)
+                liberties = frozenset(r for r in region if board[r] == go.EMPTY)
+                # reached -> set of bordering chains?
+                chains = frozenset(lib_tracker.group_index[s] for s in reached)
+                new_region = Region(curr_region_id, frozenset(region), liberties, chains, color)
+                tracker.regions[curr_region_id] = new_region
+                for s in region:
+                    tracker.region_index[s] = curr_region_id
+                go.place_stones(board, go.FILL, region)
 
-        tracker.max_group_id = curr_group_id
+        tracker.lib_tracker = lib_tracker
+        tracker.max_region_id = curr_region_id
         return tracker
 
-
-def find_all_enclosed_regions(board, player):
-    """ find black (player) enclosed regions
-    """
-    lib_tracker = LibertyTracker.from_board(board)
-    # for all empty spots, find maximal region which will be bounded by black
-    # pretty much from_board() logic
+    def eliminate(self, color):
+        """ find pass-alive chains for color
+        Benson's algorithm
+        """
+        for gid, group in self.lib_tracker.regions.items():
+            if group.color != color:
+                continue
+            # see if it has at least two (small) vital regions
 
 
 
@@ -94,9 +110,9 @@ class TestLibertyTracker(test_utils.MinigoUnitTest):
             XXXX..O..
         ''' + self.EMPTY_ROW * 6)
         tracker = PassAliveTracker.from_board(board, go.BLACK)
-        assert len(tracker.groups) == 3
-        for gid, group in tracker.groups.items():
-            print(gid, group.color, len(group.stones), len(group.liberties))
+        assert len(tracker.regions) == 3
+        for rid, region in tracker.regions.items():
+            print(rid, region.color, len(region.stones), len(region.liberties), region.chains)
 
     def test_not_pass_alive(self):
         """ two black chains, two eyes: one vital to both, one (two spaces) vital to one only
