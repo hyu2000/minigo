b426becad8d376dac7cb308f50085b2347504cd9
Hua Yu <hyu2000@gmail.com>
2022-08-12 15:03:35 -0400

    - change InitPosition to 9x9 settings - easy loading kata net

--- run_selfplay.py
+++ run_selfplay.py
@@ -9,6 +9,7 @@ import mcts
 import myconf
 from selfplay import run_game, create_dir_if_needed
 import k2net as dual_net
+from katago.analysis_engine import KataDualNetwork, KataModels
 from absl import logging, app, flags
 
 from utils import grouper
@@ -21,12 +22,12 @@ FLAGS = flags.FLAGS
 
 class InitPositions:
     """
-    limit to lower-left-down-center triangle: C3, D3, E3, D4, E4, E5
-    open_moves, open_probs = ['C3', 'D3', 'E3', 'D4', 'E4', 'E5'], np.ones(6) / 6
+    9x9: limit to lower-left-down-center triangle: C3, D3, E3, D4, E4, E5
+    5x5:    open_moves, open_probs = ['C2', 'B2'], np.ones(2) / 2
     """
     def __init__(self, open_moves: Optional[List], open_probs: Optional[List]):
         if not open_moves:
-            open_moves, open_probs = ['C2', 'B2'], np.ones(2) / 2
+            open_moves, open_probs = ['C3', 'D3', 'E3', 'D4', 'E4', 'E5'], np.ones(6) / 6
         assert len(open_moves) == len(open_probs)
 
         self.init_positions = [go.Position().play_move(coords.from_gtp(move)) for move in open_moves]
@@ -36,15 +37,24 @@ class InitPositions:
         return np.random.choice(self.init_positions, p=self.open_probs)
 
 
-def play_games(num_games=500):
-    """ """
-    model_file = FLAGS.load_file
+def load_kata_network(model_file):
+    return KataDualNetwork(model_file)
+
+
+def load_k2net(model_file):
     if model_file:
         logging.info('loading %s', model_file)
         network = dual_net.DualNetwork(model_file)
     else:
         logging.info('use DummyNetwork')
         network = dual_net.DummyNetwork()
+    return network
+
+
+def play_games(num_games=500):
+    """ """
+    network = load_k2net(FLAGS.load_file)
+    # network = load_kata_network(KataModels.MODEL_B6_4k)
 
     create_dir_if_needed(selfplay_dir=FLAGS.selfplay_dir, holdout_dir=FLAGS.holdout_dir,
                          sgf_dir=FLAGS.sgf_dir)
@@ -94,18 +104,18 @@ def main(argv):
 
 
 def main_local(argv):
-    FLAGS.load_file = f'{myconf.MODELS_DIR}/model13_epoch2.h5'
+    FLAGS.load_file = f'{myconf.MODELS_DIR}/model1_epoch5.h5'
     FLAGS.sgf_dir = f'{myconf.SELFPLAY_DIR}/sgf'
     FLAGS.selfplay_dir = f'{myconf.SELFPLAY_DIR}/train'
     FLAGS.holdout_dir = f'{myconf.SELFPLAY_DIR}/val'
-    FLAGS.num_readouts = 100
+    FLAGS.num_readouts = 50
     FLAGS.parallel_readouts = 16
     FLAGS.holdout_pct = 0
     FLAGS.softpick_move_cutoff = 6
-    FLAGS.dirichlet_noise_weight = 0.25
+    FLAGS.dirichlet_noise_weight = 0.0
     FLAGS.resign_threshold = -1.0
-    play_games(num_games=6)
+    play_games(num_games=2)
 
 
 if __name__ == '__main__':
-    app.run(main)
+    app.run(main_local)
