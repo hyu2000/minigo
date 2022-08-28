d5a363bb5049f22deac92bb158dfe2c9cff6190c
Hua Yu <hyu2000@gmail.com>
2022-08-27 22:28:44 -0400

    web_demo 9x9 board

--- webapi/web_demo.py
+++ webapi/web_demo.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import jsonify, request
 from flask import redirect, url_for
+import go
 import web_player
 import myconf
 from k2net import DualNetwork
@@ -10,14 +11,14 @@ import absl.app
 
 static_path = '/Users/hyu/PycharmProjects/dlgo/deep_learning_and_the_game_of_go/code/dlgo/httpfrontend/static'
 app = Flask(__name__, static_folder=static_path)
-player = None
+player = None  # type: web_player.WebPlayer
 
 
 def init():
     # needs flags
     global player
-    model_id = 'model10_epoch2'
-    dnn = DualNetwork(f'{myconf.EXP_HOME}/models-old/model15_epoch2.h5')
+    model_id = 'model1_epoch16'
+    # dnn = DualNetwork(f'{myconf.EXP_HOME}/models-old/model15_epoch2.h5')
     dnn = DualNetwork(f'{myconf.EXP_HOME}/checkpoints/{model_id}.h5')
     logging.info('mcts %s #readouts=%d', model_id, flags.FLAGS.num_readouts)
     player = web_player.WebPlayer(dnn)
@@ -25,14 +26,15 @@ def init():
 
 @app.route('/')
 def index():
-    return redirect(url_for('static', filename='play_agents_5.html'))
+    # return redirect(url_for('static', filename='play_agents_5.html'))
+    return redirect(url_for('static', filename='play_agents_9.html'))
 
 
 @app.route('/select-move/<bot_name>', methods=['POST'])
 def select_move(bot_name):
     content = request.json
     board_size, moves_history = content['board_size'], content['moves']
-    assert board_size == 5
+    assert board_size == go.N
 
     move = player.select_move(moves_history)
 
