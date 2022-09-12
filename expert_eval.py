530ac7a83406412f4a998ba8a25cb5db57d96abe
Hua Yu <hyu2000@gmail.com>
2022-09-12 13:44:56 -0400

    wip -- favor np array ops; roughly working, found positive surprises in both cases

--- expert_eval.py
+++ expert_eval.py
@@ -4,6 +4,7 @@ import os
 from typing import List
 import attr
 import numpy as np
+import pandas as pd
 
 import coords
 import go
@@ -18,10 +19,13 @@ class ChangePoint:
     i: int
     delta: float
 
+    def __repr__(self):
+        return f'Change(i={self.i}, {self.delta:.2f})'
+
 
 class ExpertReviewer:
     CROSSOVER_MARGIN_THRESH = 0.03
-    JIGA = 0.5
+    JIGO = 0.5
 
     def __init__(self):
         self.kata = KataEngine().start()
@@ -41,8 +45,9 @@ class ExpertReviewer:
         #         cps.append(ChangePoint(i, delta))
 
         wr_arr = np.array(black_winrates)
-        wr_signs = np.sign(wr_arr - ExpertReviewer.JIGA)
+        wr_signs = np.sign(wr_arr - ExpertReviewer.JIGO)
         wr_diff = np.diff(wr_arr)
+        # note this includes positive surprises as well
         cps = np.where((np.diff(wr_signs) != 0) & (np.abs(wr_diff) > ExpertReviewer.CROSSOVER_MARGIN_THRESH))
         # np.where returns a tuple of arrays: (array([0, 1, 4, 5]),)
         assert len(cps) == 1
@@ -56,9 +61,7 @@ class ExpertReviewer:
         reader = SGFReader.from_file_compatible(sgf_fname)
 
         moves = []
-        player_moves = []  # type: List[PlayerMove]
         for pwc in reader.iter_pwcs():
-            player_moves.append(go.PlayerMove(pwc.position.to_play, pwc.next_move))
             move = [go.color_str(pwc.position.to_play)[0], coords.to_gtp(pwc.next_move)]
             moves.append(move)
 
@@ -67,28 +70,64 @@ class ExpertReviewer:
         responses = self.kata.analyze(arequest)
 
         black_winrates = [RootInfo.from_dict(x.rootInfo).winrate for x in responses]
-        print(', '.join([f'{x:.2f}' for x in black_winrates]))
-        self.detect_crossover(black_winrates)
+        # print(', '.join([f'{x:.2f}' for x in black_winrates]))
+        cps = self.detect_crossover(black_winrates)
+        return cps
 
 
 def game_blunder_review(sgfs_dir):
     """ kata review #blunders stats by both sides, ordered by pos.n
     """
-    kata = KataEngine()
+    MAX_MOVE_NUMBER = 80 - 1
+    reviewer = ExpertReviewer()
+
+    num_games = 0
+    mistakes_by_move = np.zeros((2, MAX_MOVE_NUMBER + 1), dtype=int)
     for sgf_fname in os.listdir(f'{sgfs_dir}'):
         if not sgf_fname.endswith('.sgf'):
             continue
 
+        num_games += 1
+        cps = reviewer.review_a_game(sgf_fname)
+        # todo this is testing positive / negative surprise rather than black/white
+        black_mistakes = [x.i for x in cps if x.delta < 0]
+        mistakes_by_move[0, np.minimum(black_mistakes, MAX_MOVE_NUMBER)] += 1
+        white_mistakes = [x.i for x in cps if x.delta > 0]
+        mistakes_by_move[1, np.minimum(white_mistakes, MAX_MOVE_NUMBER)] += 1
 
-def test_review_game():
-    sgf_fname = f'{myconf.EXP_HOME}/eval_bots-model2/sgfs-model1-vs-model2/model1_epoch5#100-vs-model2_epoch2#200-15-65075018704.sgf'
+    df = pd.DataFrame(mistakes_by_move, index=['black', 'white'])
+    print(f'Total {num_games} games')
+    print(df)
 
-    reviewer = ExpertReviewer()
-    reviewer.review_a_game(sgf_fname)
+
+def test_crossover_detect0():
+    winrates = [0.53, 0.44, 0.5, 0.51, 0.59, 0.49, 0.67, 0.6, 0.67]
+    crosses = ExpertReviewer.detect_crossover(winrates)
+    print(crosses)
+    assert len(crosses) == 4
 
 
 def test_crossover_detect():
-    winrates = [0.53, 0.44, 0.5, 0.51, 0.59, 0.49, 0.67, 0.6, 0.67]
-    # winrate = [0.54, 0.51, 0.52, 0.51, 0.59, 0.48, 0.65, 0.64, 0.57, 0.53, 0.71, 0.69, 0.66, 0.69, 0.68, 0.32, 0.79, 0.48, 0.48, 0.46, 0.86, 0.81, 0.85]
+    winrates = [0.54, 0.51, 0.52, 0.51, 0.59, 0.48, 0.65, 0.64, 0.57, 0.53,
+                0.71, 0.69, 0.66, 0.69, 0.68, 0.32, 0.79, 0.48, 0.48, 0.46,
+                0.86, 0.81, 0.85]
     crosses = ExpertReviewer.detect_crossover(winrates)
     print(crosses)
+
+
+def test_review_a_game():
+    sgf_fname = f'{myconf.EXP_HOME}/eval_bots-model2/sgfs-model1-vs-model2/model1_epoch5#100-vs-model2_epoch2#200-15-65075018704.sgf'
+
+    reviewer = ExpertReviewer()
+    cps = reviewer.review_a_game(sgf_fname)
+
+    print(cps)
+    black_flips = [x.delta for x in cps if x.i % 2 == 0]
+    white_flips = [x.delta for x in cps if x.i % 2 == 1]
+    print('black', black_flips)
+    print('white', white_flips)
+
+
+def test_review_games():
+    sgfs_dir = ''
+    game_blunder_review(sgfs_dir)
