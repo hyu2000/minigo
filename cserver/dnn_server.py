96b3aa203362271bb2b32eebba0fc8da493ff2d5
Hua Yu <hyu2000@gmail.com>
2022-10-01 15:57:15 -0400

    added caching; quite some work to re-assemble output

--- cserver/dnn_server.py
+++ cserver/dnn_server.py
@@ -1,6 +1,11 @@
+"""
+- go.Position is heavy. we pass it around so server side code is easy (hash, etc.)
+- zmq: can we peek into the queue to batch things across requests, now that we have cache?
+"""
 import pickle
 import logging
 import multiprocessing as mp
+from typing import List
 
 import numpy as np
 import zmq
@@ -37,6 +42,11 @@ class DNNServer:
         self.dnn = DualNetwork(model_fname)
         self.port = port
 
+        self.cache = dict()
+        self._num_req_positions = 0
+        self._num_pos_evals = 0
+
+        # start it now!
         self.start()
 
     def start(self):
@@ -48,17 +58,44 @@ class DNNServer:
         while True:
             message = socket.recv()
             if len(message) == 0:
-                print('server exiting')
+                self._summarize()
                 break
 
             data = pickle.loads(message)
             assert isinstance(data, tuple)
+            resp = self._handle_call(*data)
 
-            pos = data[0]
-            assert isinstance(pos, go.Position)
-            resp = self.dnn.run(pos)
-            logging.info(f'raw_value=%.1f', resp[1])
             socket.send(pickle.dumps(resp))
+
+    def _handle_call(self, *args):
+        pos_list = args[0]  # type: List[go.Position]
+        assert isinstance(pos_list, list)
+
+        zhashes = [pos.zobrist_hash for pos in pos_list]
+        results = [self.cache.get(x) for x in zhashes]
+
+        idx_to_calc = [i for i, result in enumerate(results) if result is None]
+        pos_to_calc = [pos_list[i] for i in idx_to_calc]
+        priors, values = self.dnn.run_many(pos_to_calc)
+        # cache
+        for i, pos in enumerate(pos_to_calc):
+            # make a ndarray.copy for caching
+            self.cache[pos.zobrist_hash] = (priors[i].copy(), values[i])
+
+        # backfill results, assemble response
+        for i, idx in enumerate(idx_to_calc):
+            results[idx] = (priors[i], values[i])
+        all_priors = np.stack([result[0] for result in results])
+        all_values = np.stack([result[1] for result in results])
+        logging.info(f'%d -> %s %s', len(pos_list), all_priors.shape, all_values.shape)
+
+        self._num_req_positions += len(pos_list)
+        self._num_pos_evals += len(pos_to_calc)
+        return all_priors, all_values
+
+    def _summarize(self):
+        logging.info('server exiting')
+        logging.info(f'Total %d entries, #pos={self._num_req_positions}, #calc={self._num_pos_evals}', len(self.cache))
 
 
 def start_server_remote(model_fname, port):
@@ -69,15 +106,12 @@ def start_server_remote(model_fname, port):
 def test_server():
     start_server_remote(f'{myconf.MODELS_DIR}/model5_epoch2.h5', SERVER_PORT)
     stub = Stub(SERVER_PORT)
-    pos0 = go.Position()
-    priors, raw_value = stub.remote_call(pos0)
-    best_c = np.argmax(priors)
-    print(coords.flat_to_gtp(best_c), raw_value)
 
+    pos0 = go.Position()
     pos1 = pos0.play_move(coords.from_gtp('D4'))
-    priors, raw_value = stub.remote_call(pos1)
-    best_c = np.argmax(priors)
-    print(coords.flat_to_gtp(best_c), raw_value)
-    # print(type(priors), len(priors), raw_value)
+    pos2 = pos1.play_move(coords.from_gtp('E5'))
+    priors, raw_values = stub.remote_call([pos0, pos1, pos2])
+    best_moves = [coords.flat_to_gtp(x) for x in np.argmax(priors, axis=1)]
+    print(best_moves, raw_values)
 
     stub.send_eof()
