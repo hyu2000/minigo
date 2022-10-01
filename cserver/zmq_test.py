c26685847607796b239c26f60306bbe97e8a661e
Hua Yu <hyu2000@gmail.com>
2022-10-01 10:06:55 -0400

    a bit abstraction

--- cserver/zmq_test.py
+++ cserver/zmq_test.py
@@ -5,12 +5,15 @@ import numpy as np
 import pandas as pd
 import zmq
 import multiprocessing as mp
+import logging
+import go
 
 
 def start_server(port: int):
     context = zmq.Context()
     socket = context.socket(zmq.REP)
-    socket.bind("tcp://*:%s" % port)
+    # was "*:port"
+    socket.bind(f"tcp://127.0.0.1:{port}")
 
     while True:
         #  Wait for next request from client
@@ -21,12 +24,18 @@ def start_server(port: int):
             break
 
         data = pickle.loads(message)
-        print(type(data), len(data))
+        assert isinstance(data, tuple)
+        print(len(data), type(data[0]))
+
         time.sleep(0.1)
-        socket.send(b"World from server")
+        if len(data) == 2:
+            resp = b"World from server"
+        else:
+            resp = np.ones((2, 3))
+        socket.send(pickle.dumps(resp))
 
 
-def start_client(server_port: int):
+def start_client0(server_port: int):
     context = zmq.Context()
     print("Connecting to server...")
     socket = context.socket(zmq.REQ)
@@ -44,7 +53,35 @@ def start_client(server_port: int):
     socket.send(b'')
 
 
-def test_run():
+class Stub:
+    def __init__(self, server_port: int):
+        context = zmq.Context()
+        self.socket = context.socket(zmq.REQ)
+        self.socket.connect(f"tcp://localhost:{server_port}")
+
+    def remote_call(self, *args):
+        payload = pickle.dumps(args)
+        self.socket.send(payload)
+        message = self.socket.recv()
+        result = pickle.loads(message)
+        logging.info('sent %d bytes, response %d bytes', len(payload), len(message))
+        return result
+
+    def send_eof(self):
+        self.socket.send(b'')
+
+
+def start_client(server_port):
+    stub = Stub(server_port)
+    pos = go.Position()
+    result = stub.remote_call(pos)
+    print(result)
+    result = stub.remote_call(np.ones(3))
+    print(result)
+    stub.send_eof()
+
+
+def test_run0():
     port = 5555
     server_proc = mp.Process(target=start_server, args=(port,))
     server_proc.start()
