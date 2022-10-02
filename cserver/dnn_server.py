7fe6c57e7f914af6ef143088548342b33d467276
Hua Yu <hyu2000@gmail.com>
2022-10-02 11:32:22 -0400

    added handling for remote calling a couple methods: just use an int as selector

--- cserver/dnn_server.py
+++ cserver/dnn_server.py
@@ -5,7 +5,8 @@
 import pickle
 import logging
 import multiprocessing as mp
-from typing import List
+import sys
+from typing import List, Tuple
 
 import numpy as np
 import zmq
@@ -19,13 +20,21 @@ from k2net import DualNetwork
 SERVER_PORT = 5555
 
 
-class Stub:
-    def __init__(self, server_port: int):
+class RemoteMethods:
+    RUN_MANY = 1
+
+
+class DNNStub:
+    def __init__(self, server_port: int = SERVER_PORT):
         context = zmq.Context()
         self.socket = context.socket(zmq.REQ)
         self.socket.connect(f"tcp://localhost:{server_port}")
 
-    def remote_call(self, *args):
+    def run_many(self, positions: List[go.Position]) -> Tuple[np.ndarray, np.ndarray]:
+        """ stub for dnn.run_many() """
+        return self._remote_call(RemoteMethods.RUN_MANY, positions)
+
+    def _remote_call(self, *args):
         payload = pickle.dumps(args)
         self.socket.send(payload)
         message = self.socket.recv()
@@ -38,7 +47,8 @@ class Stub:
 
 
 class DNNServer:
-    def __init__(self, model_fname, port: int):
+    """ centralized DNN compute server with caching """
+    def __init__(self, model_fname, port: int = SERVER_PORT):
         self.dnn = DualNetwork(model_fname)
         self.port = port
 
@@ -63,12 +73,16 @@ class DNNServer:
 
             data = pickle.loads(message)
             assert isinstance(data, tuple)
-            resp = self._handle_call(*data)
+            # dispatch
+            method_idx = data[0]
+            if method_idx == RemoteMethods.RUN_MANY:
+                resp = self._run_many_with_cache(data[1])
+            else:
+                raise Exception(f'Invalid remote method: {method_idx}')
 
             socket.send(pickle.dumps(resp))
 
-    def _handle_call(self, *args):
-        pos_list = args[0]  # type: List[go.Position]
+    def _run_many_with_cache(self, pos_list: List[go.Position]):
         assert isinstance(pos_list, list)
 
         zhashes = [pos.zobrist_hash for pos in pos_list]
@@ -107,15 +121,24 @@ def start_server_remote(model_fname, port):
 
 def test_server():
     start_server_remote(f'{myconf.MODELS_DIR}/model5_epoch2.h5', SERVER_PORT)
-    stub = Stub(SERVER_PORT)
+    stub = DNNStub(SERVER_PORT)
 
     pos0 = go.Position()
     pos1 = pos0.play_move(coords.from_gtp('D4'))
     pos2 = pos1.play_move(coords.from_gtp('E5'))
     positions = [pos0, pos1, pos2]
     for i in range(3):
-        priors, raw_values = stub.remote_call(positions[:i+1])
+        priors, raw_values = stub.run_many(positions[:i+1])
         best_moves = [coords.flat_to_gtp(x) for x in np.argmax(priors, axis=1)]
-        logging.info(f'try{i}: {best_moves}, {raw_values}')
+        logging.info(f'try {i}: {best_moves}, {raw_values}')
 
     stub.send_eof()
+
+
+def start_server(argv):
+    port = SERVER_PORT if len(argv) == 1 else argv[1]
+    DNNServer(argv[0], port)
+
+
+if __name__ == '__main__':
+    start_server(sys.argv)
