ffad41ff40e295be30072612f1511d7e729e24ee
Hua Yu <hyu2000@gmail.com>
2023-08-04 16:02:51 -0400

    helper code to determine basics of a puzzle

--- play_puzzle1.py
+++ play_puzzle1.py
@@ -1,9 +1,94 @@
+from collections import namedtuple
+from typing import Tuple
+
+import numpy as np
+
 import coords
+import go
 import k2net as dual_net
 import myconf
 from sgf_wrapper import SGFReader
 from strategies import MCTSPlayer
-from absl import logging
+from absl import logging, app
+
+
+class BBox(namedtuple('BBox', ['row0', 'col0', 'row1', 'col1'])):
+    pass
+
+
+class LnDPuzzle:
+    # tolerance to the edge of the board
+    SNAP_EDGE_THRESH = 1
+
+    @staticmethod
+    def snap(k: int, to: int):
+        return to if abs(k - to) <= LnDPuzzle.SNAP_EDGE_THRESH else k
+
+    @classmethod
+    def find_bound_for(cls, board: np.array, side: int) -> BBox:
+        """
+        """
+        stone_coords = np.argwhere(board == side)
+        imin, jmin = stone_coords.min(axis=0)
+        imax, jmax = stone_coords.max(axis=0)
+        imin, jmin = cls.snap(imin, 0), cls.snap(jmin, 0)
+        imax, jmax = cls.snap(imax, go.N), cls.snap(jmax, go.N-1)
+        return BBox(imin, jmin, imax, jmax)
+
+    @staticmethod
+    def enclosing_box(a: BBox, b: BBox) -> BBox:
+        return BBox(min(a.row0, b.row0), min(a.col0, b.col0), max(a.row1, b.row1), max(a.col1, b.col1))
+
+    @classmethod
+    def solve_boundary(cls, board: np.array):
+        """ given a puzzle board, figure out who's surrounding, corner/center area (for the surrounded)
+
+        Attacker is more on the peripheral, or has more stones there (maybe by a weighted sum)
+        """
+        black_boundary = cls.find_bound_for(board, go.BLACK)
+        white_boundary = cls.find_bound_for(board, go.WHITE)
+        if black_boundary == white_boundary:
+            print('white_boundary == black_boundary: cannot figure out attacker/defender')
+            return black_boundary, white_boundary, 0
+        enclosure = cls.enclosing_box(black_boundary, white_boundary)
+        attack_side = 0
+        if enclosure == black_boundary:
+            attack_side = go.BLACK
+        elif enclosure == white_boundary:
+            attack_side = go.WHITE
+        if attack_side == 0:
+            print('cannot figure out which side is bigger')
+            return black_boundary, white_boundary, attack_side
+        return black_boundary, white_boundary, attack_side
+
+
+def rect_mask(i0, i1, j0, j1) -> np.array:
+    """ all inclusive """
+    mask = np.zeros((go.N, go.N), dtype=np.int8)
+    mask[i0:i1+1, j0:j1+1] = 1
+    return mask
+
+
+def mask_to_policy(board: np.array) -> np.array:
+    """ return a flattened policy, always allow pass """
+    return np.append(board.flatten(), 1)
+
+
+def test_mask():
+    print()
+    mask0 = rect_mask(0, 3, 3, 8)
+    print(mask0)
+
+
+def test_corner_area():
+    sgf_fname = '/Users/hyu/Downloads/go-puzzle9/Amigo no igo - 詰碁2023 - Life and Death/総合問題４級.sgf'
+    reader = SGFReader.from_file_compatible(sgf_fname)
+    pos = reader.first_pos()
+    black_box, white_box, attack_side = LnDPuzzle.solve_boundary(pos.board)
+    assert attack_side == go.BLACK
+    print("black", black_box)
+    print("white", white_box)
+    # white policy area: 0..3, 3..8
 
 
 def play_puzzle():
@@ -20,6 +105,7 @@ def play_puzzle():
     sgf_fname = '/Users/hyu/Downloads/go-puzzle9/Amigo no igo - 詰碁2023 - Life and Death/総合問題４級.sgf'
     reader = SGFReader.from_file_compatible(sgf_fname)
     pos = reader.first_pos()
+    white_mask = mask_to_policy(rect_mask(0, 3, 3, 8))
 
     player.initialize_game(pos)
     # Must run this once at the start to expand the root node.
@@ -28,7 +114,7 @@ def play_puzzle():
     prob, val = dnn.run(first_node.position)
     first_node.incorporate_results(prob, val, first_node)
 
-    for i in range(1):
+    for i in range(6):
         # dnn prediction
         move_probs, val_estimate = dnn.run(pos)
         move_dnn = coords.flat_to_gtp(move_probs.argmax())
@@ -40,11 +126,13 @@ def play_puzzle():
             # value output is garbage right now; also not in the expected +/-1 form
             active.tree_search()
 
-        move = active.pick_move()[1]
-        logging.info('%d %d: dnn picks %s, val=%.1f, mcts picks %s', pos.n, pos.to_play,
-                     move_dnn, val_estimate, coords.to_gtp(move))
+        move_global = active.pick_move()[1]
         pi = active.root.children_as_pi()
         print(pi[:81].reshape((9, 9)))
+        pi *= white_mask
+        move = coords.from_flat(pi.argmax())
+        logging.info('%d %s: dnn picks %s, val=%.1f, global %s -> masked %s', pos.n, go.color_str(pos.to_play),
+                     move_dnn, val_estimate, coords.to_gtp(move_global), coords.to_gtp(move))
 
         active.play_move(move)
         pos = pos.play_move(move)
@@ -53,5 +141,10 @@ def play_puzzle():
             break
 
 
-if __name__ == '__main__':
+def main(argv):
     play_puzzle()
+
+
+if __name__ == '__main__':
+    app.run(main)
+
