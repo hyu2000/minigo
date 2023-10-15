cd6fa7ac2f4acd7a9072af41ac8e2a14659d7b55
Hua Yu <hyu2000@gmail.com>
2023-10-15 10:43:41 -0400

    minor

--- train_pipe.sh
+++ train_pipe.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 
+set -e  # exit the script if any command fails
+
 # DRIVE_HOME=/content/drive/MyDrive/dlgo
 DRIVE_HOME=/Users/hyu/PycharmProjects/dlgo/9x9
 PYTHON=/Users/hyu/anaconda/envs/tf2/bin/python3
 # PYTHON=echo   # testing
 MINIGO=/Users/hyu/PycharmProjects/dlgo/minigo
 
-set -e  # exit the script if any command fails
-
 export BOARD_SIZE=9
 
 MODEL_DIR="${DRIVE_HOME}/checkpoints"
+EPOCH=2
 
 
 # main selfplay iterations
-for i in {10..11}
+for i in {7..12}
 do
   SELFPLAY_DIR="${DRIVE_HOME}/selfplay${i}p"
   echo "selfplay puzzles: ${SELFPLAY_DIR}"
   mkdir -p ${SELFPLAY_DIR}
 
-  for worker in {1..2}
+  for worker in {1..3}
   do
     ${PYTHON} ${MINIGO}/run_selfplay.py \
       puzzles \
@@ -35,8 +36,8 @@ do
       --dirichlet_noise_weight=0.25 \
       --reduce_symmetry_before_move=0 \
       --num_readouts=200 \
-      --num_games=400 \
-      --load_file="${MODEL_DIR}/model${i}_2.mlpackage" \
+      --num_games=300 \
+      --load_file="${MODEL_DIR}/model${i}_{EPOCH}.mlpackage" \
       --resign_threshold=-1 \
       2>&1 | tee "${SELFPLAY_DIR}/run_selfplay${worker}.log" &
     done
@@ -61,10 +62,10 @@ do
       --holdout_pct=0 \
       --softpick_move_cutoff=6 \
       --dirichlet_noise_weight=0.25 \
-      --num_readouts=200 \
-      --reduce_symmetry_before_move=0 \
-      --num_games=800 \
-      --load_file="${MODEL_DIR}/model${i}_2.mlpackage" \
+      --num_readouts=400 \
+      --reduce_symmetry_before_move=3 \
+      --num_games=1000 \
+      --load_file="${MODEL_DIR}/model${i}_{EPOCH}.mlpackage" \
       --resign_threshold=-1 \
       2>&1 | tee "${SELFPLAY_DIR}/run_selfplay${worker}.log" &
     done
@@ -83,17 +84,10 @@ do
   done
 
   # save data to drive?
-#  if [ $? -ne 0 ]; then
-#      echo "enhance_ds ${SELFPLAY_DIR} failed"
-#      exit 1
-#  fi
 
   TRAIN_LOG_DIR="${DRIVE_HOME}/selfplay${i}f"
-  ${PYTHON} ${MINIGO}/run_train.py "${DRIVE_HOME}/selfplay${i}[pf]/enhance/train" ${MODEL_DIR} "${i}_2" 2>&1 | tee "${TRAIN_LOG_DIR}/train${i}.log"
-
-#  if [ $? -ne 0 ]; then
-#      echo "run_train ${i} failed"
-#      exit 1
-#  fi
+  ${PYTHON} ${MINIGO}/run_train.py "${DRIVE_HOME}/selfplay${i}[pf]/enhance/train" ${MODEL_DIR} "${i}_{EPOCH}" 2>&1 | tee "${TRAIN_LOG_DIR}/train${i}.log"
 
 done
+
+echo "Done with train_pipe iter ${i}"
